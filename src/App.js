d33ab0280a546de6dbbeeb2bdd23c8988cff9293
Ian Reed <opa1164@yahoo.com>
2021-02-19 13:31:12 -0600

    Create a CodeGrade submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character.js';
+const { default: axios } = require("axios");
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,34 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const[characters, setCharacters] = useState([]);
 
+  useEffect(()=> {
+    axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+      setCharacters(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+
+  const charContainer = {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    width: "50%",
+    padding:"20px",
+    margin: "0 auto",
+  }
+
+  console.log(characters);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div style = {charContainer}> 
+        {characters.map(id =>
+          <Character character = {id}/>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import { withTheme } from 'styled-components';
 // Write your Character component here
+
+const Character = (props) => {
+
+    const {character} = props;
+    
+    const style = {
+        backgroundColor: "white",
+        padding: "0 10px",
+        margin: "10px auto",
+        display: "flex",
+        borderRadius: "5px",
+        justifyContent: "space-between",
+        alignItems: "center",
+    }
+
+
+    return(
+    <div style = {style}>
+        <h2>{character.name}</h2>
+        <p>Birth year: {character.birth_year}</p>
+    </div>
+    );
+};
+
+export default Character;
